0b5f9941807bc16d5fffb62fb75abade10eba6c5
Anderson Silva <anderson.silva@indigosoft.com.br>
2020-03-26 17:05:53 -0300

    subindo meu ambiente de dev

---
+++ barberShopControl/core/criar_cadastro.py
@@ -0,0 +1,4 @@
+from . import Usuario
+
+usuario = Usuario()
+print(usuario)
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\anderson.silva\Desktop\Barbearia-Amizades-develop\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
