d50efbec78ff0119fd1198269288f4cdd5184f29
Lenildo Nascimento <lensilva@uolinc.com>
2020-04-16 20:07:57 -0300

    Estrutura do login finalizada.

--- barberShopControl/core/urls.py
+++ barberShopControl/core/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.base, name="base"),
+    path('', views.login, name="login"),
 ]
--- barberShopControl/core/views.py
+++ barberShopControl/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def base(request):
-    return render(request, 'base.html')
+def login(request):
+    return render(request, 'login.html')
