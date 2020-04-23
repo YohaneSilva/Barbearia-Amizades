1dcdbd598e9a5882abc6b5d39b66a9973052d629
Lenildo Nascimento <lensilva@uolinc.com>
2020-04-23 10:02:00 -0300

    Alterado estrutura de pastas e iniciado desenvolvimento dos templates do módulo Minha Conta.

--- barberShopControl/core/urls.py
+++ barberShopControl/core/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.loginPrincipal, name="loginPrincipal"),
+    path('login/', views.acesso, name="acesso"),
     path('login/recuperarsenha/', views.recuperarSenha, name="recuperarSenha"),
     path('login/criarconta/', views.criarConta, name="criarConta"),
-    path('minhaconta/dashboard/', views.dashboar, name="dashboard"),
-    path('minhaconta/servicos/', views.servicos, name="servicos"),
+    path('minhaconta/dashboard/', views.dashboard, name="dashboard"),
+    path('minhaconta/servicos/cadastro', views.servicos, name="servicos"),
 ]
